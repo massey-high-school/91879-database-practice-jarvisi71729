39f74a582e6b6dd59b2ae613809ed75b3fa76934
Iszac <58008716+jarvisi71729@users.noreply.github.com>
2020-08-03 09:55:10 +1200

    3/08/2020

---
+++ topbit.php
@@ -0,0 +1,60 @@
+<!DOCTYPE HTML>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Put Content Here">
+    <meta name="keywords" content="Put keywords here">
+    <meta name="author" content="Put your name here">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+    <title>Website Title goes here</title>
+   
+    <!-- Edit the link below / replace with your chosen google font -->
+    <link href="https://fonts.googleapis.com/css?family=Lato%7cUbuntu" rel="stylesheet">
+   
+    <!-- Edit the name of your style sheet - 'foo' is not a valid name!! -->
+    <link rel="stylesheet" href="no_side.css">
+   
+</head>
+   
+<body>
+   
+    <div class="wrapper">
+   
+        <a href="index.php">
+            <div class="box logo">
+           
+            </div>    <!-- / logo -->
+        </a>
+       
+        <div class="box banner">
+            
+            <h1>Chonkers</h1>
+
+        </div>    <!-- / banner -->
+
+        <!-- Navigation goes here.  Edit BOTH the file name and the link name -->
+        <div class="box nav">
+            <a href="index.html">Home</a> |
+            <a href="page1.html">Page 1</a> |
+            <a href="page2.html">Page 2</a> |
+            <a href="page3.html">Page 3</a> |
+            <a href="contact.html">Contact</a>
+        </div>    <!-- / nav -->        
+       
+        <div class="box side">
+       
+            <h2>Search  |   <a class="side" href="showall.php">Show All</a></h2>
+            
+            <i>Type part of the title / author name if desired</i>
+            
+            <hr />
+            
+            Title Search<br />
+            Author Search<br />
+            Genre Search<br />
+            Rating Search
+       
+        </div>
